8e2046b4beb2b1860388b125707e8a36839365b0
MarriaMarria <maria.kononevs@gmail.com>
2021-01-24 23:05:27 +0100

    added function to shuffle the names (not yet working)

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let nameList = [];
 
+
 const initButtons =()=>{
+    let shuttledlist; 
+
     const addButton = document.querySelector('#addbtn');
     addButton.addEventListener('click', () => { 
         const inputName = document.querySelector('#name');
@@ -13,15 +16,18 @@ const initButtons =()=>{
                 inputName.value = '';
                 // listNames();
             }
-        console.log("names: ", nameList);
+
+        
+ 
+
     })
 
     const divideInGroups = document.querySelector('#dividebtn');
     divideInGroups.addEventListener('click', () => {
         const nb = document.querySelector('#nmbPpl').value;
         let result =[];
-        while(nameList.length){
-            let group = nameList.splice(0,nb);
+        while(shuffledList.length){
+            let group = shuffledList.splice(0,nb);
             result.push(group)
         }
         showResult(result);
@@ -29,7 +35,7 @@ const initButtons =()=>{
 
 }
 
-const showResult=(nameList)=>{
+const showResult=(shuffledList) => {
     const resultDiv = document.querySelector('#result');
     for(let group of nameList){
         const groupDiv = document.createElement('div');
@@ -50,61 +56,29 @@ const showResult=(nameList)=>{
 initButtons();
 
 
-// we take input from id #name, if there is a value we create a <li> element, add input to it with innerHTML, and we add new <li> to <ul>
-// then I clear input value
+
+// NB! Working shuffle function, just have to figure out where to call it from
 
 
-// make a function!!!!!
-// we grab our inputs stored in <li> and add them to one list allNames
-// const listNames = () => {
-//     const allNames = document.querySelectorAll("#storeNames li")
-//     // console.log(allNames);
-//     let listOfNames = []; // either take it out to put in global scope up 
-//     for (let i = 0; i < allNames.length; i++) {
-//         listOfNames.push(allNames[i].textContent);
-
-//     }
-//     console.log(listOfNames);
-// }
-
-
-
-
-// const shuffle = new Function(array) { // Fisher-Yates-Durstenfeld shuffle
-//     var currentIndex = array.length, temporaryValue, randomIndex;
+const shuffle = (array) => {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1; 
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+}
     
-//     // While there remain elements to shuffle...
-//     while (0 !== currentIndex) {
+    return array;
+}
+
+
+
+let shuffledList = shuffle(nameList);
+console.log("names: ", shuffle(nameList));
     
-//     // Pick a remaining element... 
-//     randomIndex = Math.floor(Math.random() * currentIndex);
-//     currentIndex -= 1;
     
-//     // And swap it with the current element. 
-//     temporaryValue = array[currentIndex];
-//     array[currentIndex] = array[randomIndex];
-//     array[randomIndex] = temporaryValue;
-//     }
-    
-//     return array;
-//     }
-    
-    // Used like so 
-// shuffle(list_of_names);
-// console.log(list_of_names);
-    
-    // const shufflebtn = document.getElementById("shuffle")
-    // shufflebtn.addEventListener("click", function() { 
-    // shuffle(list_of_names);
-    // return list_of_names
-    // });
-
-
-
-
-
-
-// when we click the button function divide in groups which takes a list of names and a number of ppl activates
 
 
 
@@ -119,24 +93,3 @@ initButtons();
 
 
 
-
-
-
-
-
-
-
-
-
-// const addButton = document.querySelector('#addbtn');
-// const inputName = document.querySelector('#name');
-// listOfNames = [];
-
-
-// addButton.addEventListener('click', () => { 
-//     let myName = inputName.value;
-//     listOfNames.push(myName);
-//     console.log(listOfNames);
-//     document.getElementById("store_names").innerHTML = listOfNames;
-
-// })
